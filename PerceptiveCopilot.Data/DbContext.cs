b5c8187c677b1233508d282f520023bc6d0903b4
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-09-21 14:50:58 +0100

    Add data project

---
+++ PerceptiveCopilot.Data/DbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PerceptiveCopilot.Data
+{
+    public class PerceptiveCopilotDbContext : DbContext
+    {
+        public DbSet<DocumentChunk> DocumentChunks { get; set; }
+    }
+}
---
+++ PerceptiveCopilot.Data/DocumentChunk.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlTypes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PerceptiveCopilot.Data
+{
+    public class DocumentChunk
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        public string DocumentName { get; set; } = string.Empty;
+        public string ContentChunk { get; set; } = string.Empty;
+        [Column(TypeName = "vector(1536)")]
+        public SqlVector<float> Embedding { get; set; }
+    }
+}
